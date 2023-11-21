8e61846c365745bbc64c0e81867ddfde43e08a2f
enjiawu <enjia.wu@gmail.com>
2023-11-22 01:33:59 +0800

    made navigation & featured video responsive

--- js/main.js
+++ js/main.js
@@ -1,12 +1,41 @@
 //Making the hamburger responsive
-//Getting the hamburger elements
-const hamburger = document.getElementsByClassName("hamburgers");
-const bars = document.getElementsByClassName("bar");
+function hamburgerClick(){ //Making the hamburger menu pop out when clicked
+    const navigationButtons = document.getElementById("navigation-buttons");  //Getting Navigation Buttons
+    const socialmediaButtons = document.getElementById("socialmedia-buttons"); //Getting Social Media Buttons
+    const NavContainer = document.getElementById("nav-container"); //Getting the hamburger nav container
 
-//Function to see if the button is clicked and respond accordingly
-function hamburgerClick() 
-{
-    if (hamburger.onclick === true){
-        alert("Button Clicked")
+    if (navigationButtons.style.display === "flex" && socialmediaButtons.style.display === "flex"){ //If both the navigation and social media buttons are visible
+        navigationButtons.style.display = "none"; //Hiding the navigation buttons
+        socialmediaButtons.style.display = "none"; //Hiding the social media buttons
+        NavContainer.style.height = "65px"; //Changing the height back to 65px
+        NavContainer.style.backgroundColor = "rgb(153, 176, 241)"; //Changing the color back to the original
+    }
+    else{
+        navigationButtons.style.display = "flex"; //Showing the navigation buttons
+        socialmediaButtons.style.display = "flex"; //Showing the social media buttons
+        NavContainer.style.height = "500px"; //Increasing the navigation's background height
+        NavContainer.style.backgroundColor = "rgb(127, 142, 184)"; //Changing the color to a darker shade
     }
 }
+
+//Making sure that the hamburger button doesnt make the display for the buttons none when the screen is expanded
+window.addEventListener("resize", function(){
+    const navigationButtons = document.getElementById("navigation-buttons");  //Getting Navigation Buttons
+    const socialmediaButtons = document.getElementById("socialmedia-buttons"); //Getting Social Media Buttons
+    const NavContainer = document.getElementById("nav-container"); //Getting the hamburger nav container
+
+
+    if (window.innerWidth > 900){
+        navigationButtons.style.display = "flex"; //Showing the navigation buttons
+        socialmediaButtons.style.display = "flex"; //Showing the social media buttons
+        NavContainer.style.height = "75px"; //Making sure the height is 75px
+        NavContainer.style.backgroundColor = "rgb(153, 176, 241)"; //Changing the color back to the original
+    }
+    else{
+        navigationButtons.style.display = "none"; //Hiding the navigation buttons
+        socialmediaButtons.style.display = "none"; //Hiding the social media buttons
+        NavContainer.style.height = "65px"; //Making sure the height is 65px
+        NavContainer.style.backgroundColor = "rgb(153, 176, 241)"; //Changing the color back to the original
+    }
+});
+
