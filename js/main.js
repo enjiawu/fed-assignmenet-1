0863351a3e99d1ee0e0a76559beb8b4276f3c679
enjiawu <enjia.wu@gmail.com>
2023-11-30 08:59:23 +0800

    changed color of website from blue to red

--- js/main.js
+++ js/main.js
@@ -9,13 +9,13 @@ function hamburgerClick(){ //Making the hamburger menu pop out when clicked
         navigationButtons.style.display = "none"; //Hiding the navigation buttons
         socialmediaButtons.style.display = "none"; //Hiding the social media buttons
         NavContainer.style.height = "65px"; //Changing the height back to 65px
-        NavContainer.style.backgroundColor = "rgb(153, 176, 241)"; //Changing the color back to the original
+        NavContainer.style.backgroundColor = "#ec6262"; //Changing the color back to the original
     }
     else{
         navigationButtons.style.display = "flex"; //Showing the navigation buttons
         socialmediaButtons.style.display = "flex"; //Showing the social media buttons
         NavContainer.style.height = "500px"; //Increasing the navigation's background height
-        NavContainer.style.backgroundColor = "rgb(127, 142, 184)"; //Changing the color to a darker shade
+        NavContainer.style.backgroundColor = "#c57777"; //Changing the color to a darker shade
     }
 }
 //Making sure that the hamburger button doesnt make the display for the buttons none when the screen is expanded
@@ -29,13 +29,13 @@ window.addEventListener("resize", function(){
         navigationButtons.style.display = "flex"; //Showing the navigation buttons
         socialmediaButtons.style.display = "flex"; //Showing the social media buttons
         NavContainer.style.height = "75px"; //Making sure the height is 75px
-        NavContainer.style.backgroundColor = "rgb(153, 176, 241)"; //Changing the color back to the original
+        NavContainer.style.backgroundColor = "#ec6262"; //Changing the color back to the original
     }
     else{
         navigationButtons.style.display = "none"; //Hiding the navigation buttons
         socialmediaButtons.style.display = "none"; //Hiding the social media buttons
         NavContainer.style.height = "65px"; //Making sure the height is 65px
-        NavContainer.style.backgroundColor = "rgb(153, 176, 241)"; //Changing the color back to the original
+        NavContainer.style.backgroundColor = "#ec6262"; //Changing the color back to the original
     }
 });
 
